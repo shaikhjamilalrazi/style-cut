47aa3ab6ff8c55f2499412d4479c884a1aabacda
shaikhjamilalrazi <shaikhjamilalrazi@gmail.com>
2021-04-18 18:56:08 +0600

    updated some issue

--- src/components/Home/Testimonial/Testimonial.js
+++ src/components/Home/Testimonial/Testimonial.js
@@ -12,6 +12,27 @@ const Testimonial = () => {
             .then((data) => setTestimonials(data));
     }, []);
 
+    const testimonialFirstHalf = testimonials.slice(
+        1,
+        Math.round(testimonials.length / 2)
+    );
+
+    let a = testimonialFirstHalf;
+    let b = testimonialsNew;
+
+    let valuesA = a.reduce(
+        (a, { _id }) => Object.assign(a, { [_id]: _id }),
+        {}
+    );
+    let valuesB = b.reduce(
+        (a, { _id }) => Object.assign(a, { [_id]: _id }),
+        {}
+    );
+    let testimonialSecondHalf = [
+        ...a.filter(({ _id }) => !valuesB[_id]),
+        ...b.filter(({ _id }) => !valuesA[_id]),
+    ];
+
     return (
         <div className="container my-5">
             <h1 className="text-center mb-5">Testimonial</h1>
@@ -42,29 +63,33 @@ const Testimonial = () => {
                                 </div>
                             </div>
 
-                            {testimonials?.slice(1).map((testimonial) => (
-                                <div
-                                    className="carousel-item"
-                                    key={testimonial._id}
-                                >
-                                    <div className="d-flex">
-                                        <img
-                                            className="img-fluid"
-                                            style={{
-                                                width: "100px",
-                                                height: "100px",
-                                                borderRadius: "100%",
-                                            }}
-                                            src={testimonial?.photo}
-                                            alt=""
-                                        />
-                                        <div className="ps-3 w-75">
-                                            <h1>{testimonial?.name}</h1>
-                                            <p>{testimonial?.description}</p>
+                            {testimonialFirstHalf
+                                ?.slice(1)
+                                .map((testimonial) => (
+                                    <div
+                                        className="carousel-item"
+                                        key={testimonial._id}
+                                    >
+                                        <div className="d-flex">
+                                            <img
+                                                className="img-fluid"
+                                                style={{
+                                                    width: "100px",
+                                                    height: "100px",
+                                                    borderRadius: "100%",
+                                                }}
+                                                src={testimonial?.photo}
+                                                alt=""
+                                            />
+                                            <div className="ps-3 w-75">
+                                                <h1>{testimonial?.name}</h1>
+                                                <p>
+                                                    {testimonial?.description}
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
                         </div>
                     </div>
                 </div>
@@ -84,39 +109,50 @@ const Testimonial = () => {
                                             height: "100px",
                                             borderRadius: "100%",
                                         }}
-                                        src={testimonialsNew[0]?.photo}
+                                        src={testimonialSecondHalf[0]?.photo}
                                         alt=""
                                     />
                                     <div className="ps-3 w-75">
-                                        <h1>{testimonialsNew[0]?.name}</h1>
-                                        <p>{testimonialsNew[0]?.description}</p>
+                                        <h1>
+                                            {testimonialSecondHalf[0]?.name}
+                                        </h1>
+                                        <p>
+                                            {
+                                                testimonialSecondHalf[0]
+                                                    ?.description
+                                            }
+                                        </p>
                                     </div>
                                 </div>
                             </div>
 
-                            {testimonialsNew?.slice(1).map((testimonial) => (
-                                <div
-                                    className="carousel-item"
-                                    key={testimonial._id}
-                                >
-                                    <div className="d-flex p-3">
-                                        <img
-                                            className="img-fluid"
-                                            style={{
-                                                width: "100px",
-                                                height: "100px",
-                                                borderRadius: "100%",
-                                            }}
-                                            src={testimonial?.photo}
-                                            alt=""
-                                        />
-                                        <div className="ps-3 w-75">
-                                            <h1>{testimonial?.name}</h1>
-                                            <p>{testimonial?.description}</p>
+                            {testimonialSecondHalf
+                                ?.slice(1)
+                                .map((testimonial) => (
+                                    <div
+                                        className="carousel-item"
+                                        key={testimonial._id}
+                                    >
+                                        <div className="d-flex p-3">
+                                            <img
+                                                className="img-fluid"
+                                                style={{
+                                                    width: "100px",
+                                                    height: "100px",
+                                                    borderRadius: "100%",
+                                                }}
+                                                src={testimonial?.photo}
+                                                alt=""
+                                            />
+                                            <div className="ps-3 w-75">
+                                                <h1>{testimonial?.name}</h1>
+                                                <p>
+                                                    {testimonial?.description}
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
                         </div>
                     </div>
                 </div>
